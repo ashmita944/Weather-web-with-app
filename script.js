729f6fcdb618e05ea07e615e20c900a351c9516f
ASHMITA <ashmita.in.com@gmail.com>
2025-04-29 21:01:45 -0400

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const Weather_body = document.querySelector('.Weather-body');
 
 
 async function checkWeather(city){
-    const api_key ="b9c6477edbe018eda132b6c7f9d74f70"
+    const api_key ="............"
     const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
